f8b70bb933b7f3a58eb8c79b47fc908c17048006
johnhodge <18251637+johnhodge@users.noreply.github.com>
2020-09-20 20:41:06 -0400

    more homepage cleanup

--- public/src/components/layout.js
+++ public/src/components/layout.js
@@ -53,26 +53,26 @@ class Layout extends React.Component {
         </h3>
       )
     }
-    return (
-      <Wrapper>
-        <div
-          style={{
-            marginLeft: `auto`,
-            marginRight: `auto`,
-            maxWidth: rhythm(24),
-            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-          }}
-        >
-          <header>{header}</header>
-          <main>{children}</main>
-        </div>
-        <Footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </Footer>
-      </Wrapper>
-    )
+    // return (
+    //   <Wrapper>
+    //     <div
+    //       style={{
+    //         marginLeft: `auto`,
+    //         marginRight: `auto`,
+    //         maxWidth: rhythm(24),
+    //         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+    //       }}
+    //     >
+    //       <header>{header}</header>
+    //       <main>{children}</main>
+    //     </div>
+    //     <Footer>
+    //       © {new Date().getFullYear()}, Built with
+    //       {` `}
+    //       <a href="https://www.gatsbyjs.org">Gatsby</a>
+    //     </Footer>
+    //   </Wrapper>
+    // )
   }
 }
 
--- public/src/pages/index.js
+++ public/src/pages/index.js
@@ -10,22 +10,22 @@ class IndexPage extends React.Component {
     const siteTitle = "Gatsby Starter Personal Website"
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <div class="container">
-          <div id="main">
-            <h1 class="headline headline_one">John Hodge</h1>
-          </div>
-          <address>
-            <div class="icons">
-              <a href="https://github.com/johnhodge" target="_blank" class="icon-link"><img class="icon" src="https://unpkg.com/simple-icons@v3/icons/github.svg" /></a><a href="https://twitter.com/hodgecity" target="_blank" class="icon-link"><img class="icon" src="https://unpkg.com/simple-icons@v3/icons/twitter.svg" /></a>
-            </div>
-          </address>
+      // <Layout location={this.props.location} title={siteTitle}>
+      //   <SEO
+      //     title="Home"
+      //     keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      //   />
+      <div class="container">
+        <div id="main">
+          <h1 class="headline headline_one">John Hodge</h1>
         </div>
-      </Layout>
+        <address>
+          <div class="icons">
+            <a href="https://github.com/johnhodge" target="_blank" class="icon-link"><img class="icon" src="https://unpkg.com/simple-icons@v3/icons/github.svg" /></a><a href="https://twitter.com/hodgecity" target="_blank" class="icon-link"><img class="icon" src="https://unpkg.com/simple-icons@v3/icons/twitter.svg" /></a>
+          </div>
+        </address>
+      </div>
+      // </Layout>
     )
   }
 }
