87a17b1a8af58184a0a8e4d1d6c9901d3f7667a3
johnhodge <18251637+johnhodge@users.noreply.github.com>
2020-09-20 20:33:04 -0400

    homepage

--- public/src/pages/index.js
+++ public/src/pages/index.js
@@ -15,22 +15,16 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
+        <div class="container">
+          <div id="main">
+            <h1 class="headline headline_one">John Hodge</h1>
+          </div>
+          <address>
+            <div class="icons">
+              <a href="https://github.com/johnhodge" target="_blank" class="icon-link"><img class="icon" src="https://unpkg.com/simple-icons@v3/icons/github.svg" /></a><a href="https://twitter.com/hodgecity" target="_blank" class="icon-link"><img class="icon" src="https://unpkg.com/simple-icons@v3/icons/twitter.svg" /></a>
+            </div>
+          </address>
+        </div>
       </Layout>
     )
   }
