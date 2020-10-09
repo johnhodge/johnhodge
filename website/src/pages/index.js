f0b1495e3fef9b0161526f24981beab11aad5d5e
johnhodge <18251637+johnhodge@users.noreply.github.com>
2020-10-09 09:04:13 -0400

    style: add fonts and shadows

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -14,7 +14,9 @@ class IndexPage extends React.Component {
         />
         <h1 class="display"><span class="hightlight">John Hodge</span></h1>
         <div class="icons">
-          <a href="https://github.com/johnhodge" target="_blank" rel="noreferrer" class="icon-link" title="Follow me on Github"><img class="icon" alt="Follow me on Github" src="https://unpkg.com/simple-icons@v3/icons/github.svg" /></a><a href="https://twitter.com/hodgecity" target="_blank" rel="noreferrer" class="icon-link" title="Peep my Tweets"><img class="icon" alt="Peep my Tweets" src="https://unpkg.com/simple-icons@v3/icons/twitter.svg" /></a>
+          <div class="icon-bucket">
+            <a href="https://github.com/johnhodge" target="_blank" rel="noreferrer" class="icon-link" title="Follow me on Github"><img class="icon" alt="Follow me on Github" src="https://unpkg.com/simple-icons@v3/icons/github.svg" /></a><a href="https://twitter.com/hodgecity" target="_blank" rel="noreferrer" class="icon-link" title="Peep my Tweets"><img class="icon" alt="Peep my Tweets" src="https://unpkg.com/simple-icons@v3/icons/twitter.svg" /></a>
+          </div>
         </div>
       </Layout>
     )
