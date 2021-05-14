0653d8ee28f6e124ac913a8273bd139a3dc0426c
johnhodge <18251637+johnhodge@users.noreply.github.com>
2021-05-13 21:23:50 -0400

    test: change rewrite rule for rewrite base :rotating_light:

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,18 +60,18 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-htaccess',
       options: {
-        RewriteBase: true,
+        RewriteBase: '/',
         https: true,
         www: true,
         SymLinksIfOwnerMatch: true,
-        host: 'www.mydomain.com', // if 'www' is set to 'false', be sure to also remove it here!
-        ErrorDocument: `
-          ErrorDocument 401 /error_pages/401.html
-          ErrorDocument 404 /error_pages/404.html
-          ErrorDocument 500 /error_pages/500.html
-        `,
+        host: 'www.johnhodge.com', // if 'www' is set to 'false', be sure to also remove it here!
+        // ErrorDocument: `
+        //   ErrorDocument 401 /error_pages/401.html
+        //   ErrorDocument 404 /error_pages/404.html
+        //   ErrorDocument 500 /error_pages/500.html
+        // `,
         redirect: [
-          'rewriteRule ^(([a-z0-9\-]+/)*[a-z0-9\-]+)$ $1/ [NC,R=301,L]',
+          // 'rewriteRule ^(([a-z0-9\-]+/)*[a-z0-9\-]+)$ $1/ [NC,R=301,L]',
           {
         //     from: 'my-domain.com',
         //     to: 'mydomain.com',
