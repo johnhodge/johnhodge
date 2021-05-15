39db81f0302b37f3d174a5fb920dbd277d178d68
johnhodge <18251637+johnhodge@users.noreply.github.com>
2021-05-14 21:46:23 -0400

    feat: add prettier and eslint support :sparkles:

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 
-import Header from "./header";
-import Footer from "./footer";
-import "../styles/index.scss";
-import styles from "./layout.module.scss";
+import Header from './header';
+import Footer from './footer';
+import '../styles/index.scss';
+import styles from './layout.module.scss';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
       <Header />
       <main
-        itemprop="mainEntity"
+        itemprop='mainEntity'
         itemscope
-        itemtype="https://schema.org/WebSite"
+        itemtype='https://schema.org/WebSite'
         className={styles.main}>
         {children}
       </main>
