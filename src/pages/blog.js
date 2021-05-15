40f50cf6ec3e0f1e47ccbd99ca4af5de59aa37f7
johnhodge <18251637+johnhodge@users.noreply.github.com>
2021-05-15 19:41:04 -0400

    refactor: blogroll into component with static query: closes #65 :package:

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,57 +1,19 @@
-import React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
+import React from 'react';
 
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import styles from "./blog.module.scss";
+import Layout from '../components/layout';
+import BlogRoll from '../components/blog-roll';
+import SEO from '../components/seo';
 
 const Blog = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
-        edges {
-          node {
-            slug
-            title
-            publishDate(formatString: "MMMM Do, YYYY")
-            heroImage {
-              file {
-                url
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
   return (
     <Layout>
       <SEO
-        metaTitle="Blog"
-        description="This blog is a space for me to log what I learn in my professional travels as a tech-wise client-facing solutions engineer."
-        pathname="/blog"
+        metaTitle='Blog'
+        description='This blog is a space for me to log what I learn in my professional travels as a tech-wise client-facing solutions engineer.'
+        pathname='/blog'
       />
       <h1>Blog.</h1>
-      <ol className={styles.posts}>
-        {data.allContentfulBlogPost.edges.map((edge, i) => {
-          return (
-            <Link className={styles.blogLink} to={`/blog/${edge.node.slug}`}>
-              <li
-                className={styles.post}
-                key={i}
-                style={{
-                  backgroundImage: `url(https:${edge.node.heroImage.file.url})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "50%",
-                  padding: "1.5rem",
-                }}
-              >
-                <h2 className={styles.blogHeader}>{edge.node.title}</h2>
-              </li>
-            </Link>
-          );
-        })}
-      </ol>
+      <BlogRoll />
     </Layout>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,17 @@
 import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import BlogRoll from '../components/blog-roll';
 import SEO from '../components/seo';
-import styles from './blog.module.scss';
 
 const Index = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
-        edges {
-          node {
-            id
-            slug
-            title
-            publishDate(formatString: "MMMM Do, YYYY")
-            heroImage {
-              file {
-                url
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
   return (
     <Layout>
       <SEO metaTitle='Home' pathname='/' />
       <h1>Hello!</h1>
       <p>I'm John, I work in solutions and I play around with FM synthesis.</p>
       <h2>Recent posts.</h2>
-      <ol className={styles.posts}>
-        {data.allContentfulBlogPost.edges.map((edge, i) => {
-          return (
-            <Link className={styles.blogLink} to={`/blog/${edge.node.slug}`}>
-              <li
-                key={i}
-                className={styles.post}
-                style={{
-                  backgroundImage: `url(https:${edge.node.heroImage.file.url})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: '50%',
-                  padding: '1.5rem',
-                }}>
-                <h2 className={styles.blogHeader}>{edge.node.title}</h2>
-              </li>
-            </Link>
-          );
-        })}
-      </ol>
+      <BlogRoll />
     </Layout>
   );
 };
