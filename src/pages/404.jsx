a0e98e85cbe4d001591408cbdc2485c7d8c11490
John <18251637+johnhodge@users.noreply.github.com>
2021-10-06 18:37:16 -0400

    create 404 page #8

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,52 +1,22 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import JsonLd from "../components/json-ld";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-
-// markup
-const NotFoundPage = () => (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  );
+const NotFoundPage = ({ location }) => (
+  <div>
+    <Layout location={location} pageTitle="404: Page not found">
+      <Seo metaTitle={`No page: ${location.pathname.replace("/", "")}`} />
+      <JsonLd>
+        {{
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          url: location.origin,
+          name: "BrightShell, LLC",
+        }}
+      </JsonLd>
+    </Layout>
+  </div>
+);
 
 export default NotFoundPage;
