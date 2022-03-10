19a66def9dfbba546806dff8b575249809d88111
John <18251637+johnhodge@users.noreply.github.com>
2022-03-10 09:36:43 -0500

    remove old meeting req page and change copy

--- src/pages/lp/request-meeting.jsx
+++ src/pages/lp/request-meeting.jsx
@@ -10,8 +10,8 @@ const Contact = ({ location }) => (
       <Seo metaTitle="Request a Meeting" location={location} />
       <JsonLd schemaType="webpage" />
       <p>
-        Please tell us a little about your place in the industry and what you
-        need help with and we&apos;ll be in touch to discuss solutions.
+        Please tell me a little about your place in the industry and what you
+        need help with and I&apos;ll be in touch to discuss solutions.
       </p>
       <MeetingRequestForm />
     </Layout>
