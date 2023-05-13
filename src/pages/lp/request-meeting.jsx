aed212900af9dc13eeae174e55dc9c2341864a11
John <18251637+johnhodge@users.noreply.github.com>
2023-05-13 08:42:58 -0400

    fix build and minor eslint issues

--- src/pages/lp/request-meeting.jsx
+++ src/pages/lp/request-meeting.jsx
@@ -11,7 +11,7 @@ const Contact = ({ location }) => (
       <JsonLd schemaType="webpage" />
       <p>
         Please share a little about your place in the industry and what you need
-        help with and let's discuss solutions.
+        help with and let&apos;s discuss solutions.
       </p>
       <MeetingRequestForm />
     </Layout>
