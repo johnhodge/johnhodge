f51dc830f2f1aa99ee2f8bc461ef9fe22f713795
John <18251637+johnhodge@users.noreply.github.com>
2023-06-21 11:59:50 -0400

    MAR-14 adjust sizing of root element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className='flex min-h-dscreen flex-col justify-center p-4'>
-      <h1 className='text-6xl font-display text-primary-600'>John Hodge</h1>
+      <h1 className='text-5xl font-display text-primary-600'>John Hodge</h1>
       <p>New website coming soon.</p>
     </main>
   );
