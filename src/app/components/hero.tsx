9a8f933a0ca1766058f3b09b6ae9f65c6dee52fb
John <18251637+johnhodge@users.noreply.github.com>
2023-06-22 16:53:44 -0400

    MAR-14 create hero component

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,12 +1,38 @@
 import type { Person } from '../types';
+import Image from 'next/image';
+import Button from './shared/button';
 
 export default async function Hero(data: Person) {
   return (
-    <section className='flex min-h-dscreen flex-col justify-center p-4'>
-      <h1 className='text-5xl font-display text-primary-600'>
-        {data.firstName} {data.lastName}
-      </h1>
-      <p>New website coming soon.</p>
-    </section>
+    <article className='flex flex-col gap-4 px-4 md:flex-row xl:px-24 xl:gap-12'>
+      <section className='basis-1/2 flex flex-col justify-evenly items-start'>
+        <h1
+          className='text-5xl font-black  xl:text-7xl'
+          dangerouslySetInnerHTML={{ __html: data.headline }}
+        />
+        <h2 className='text-2xl font-extrabold py-4'>{data.subhead}</h2>
+        <Button
+          size='large'
+          color='secondary'
+          link='/#contact'
+          width='fit'
+          text='Schedule a consultation'
+        />
+      </section>
+
+      <figure className='basis-1/2'>
+        <Image
+          src={data.headshot.url}
+          height={data.headshot.height}
+          width={data.headshot.width}
+          title={data.headshot.title}
+          alt={data.headshot.description}
+          className='aspect-ratio-1/1'
+        />
+        <figcaption className='text-primary-700 bg-primary-50 px-2'>
+          {data.headshot.description}
+        </figcaption>
+      </figure>
+    </article>
   );
 }
