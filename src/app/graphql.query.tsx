99d2a6d07051e3739620ad05832fa9d54f1d45dc
John <18251637+johnhodge@users.noreply.github.com>
2023-06-22 11:09:13 -0400

    MAR-14 add graphql for person and site meta plug site meta graphql into layout

---
+++ src/app/graphql.query.tsx
@@ -0,0 +1,132 @@
+export const GetWebsiteData = `query GetWebsiteData{
+  person(id: "2dHdqDmhdvsyfrhkYq7G8n") {
+    sys {
+      id
+    }
+    firstName
+    lastName
+    headline
+    subhead
+    headshot {
+      url
+      title
+      description
+      width
+      height
+    }
+    clients: projectsCollection(
+      limit: 6
+      where: { type_not: "Employment - W2" }
+    ) {
+      items {
+        headline
+        clientsCollection(limit: 1) {
+          items {
+            name
+            logo {
+              url
+              title
+              description
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+    employment: projectsCollection(
+      limit: 4
+      where: { type: "Employment - W2" }
+    ) {
+      items {
+        jobTitle
+        headline
+        body
+        clientsCollection(limit: 1) {
+          items {
+            name
+            logo {
+              url
+              title
+              description
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+    testimonials: testimonialsCollection(limit: 6) {
+      items {
+        excerpt
+        body
+        author {
+          firstName
+          lastName
+          headshot {
+            url
+            title
+            description
+            width
+            height
+          }
+          projectsCollection(limit: 1, where: { type: "Employment - W2" }) {
+            items {
+              jobTitle
+            }
+          }
+        }
+      }
+    }
+    skills: skillCollection(limit: 6) {
+      items {
+        title
+        headline
+        body
+        icon {
+          url
+          title
+          description
+          width
+          height
+        }
+      }
+    }
+    technology: technologyCollection(limit: 6) {
+      items {
+        title
+        headline
+        body
+        icon {
+          url
+          title
+          description
+          width
+          height
+        }
+      }
+    }
+    philosophy: philosophyCollection {
+      items {
+        title
+        headline
+        body
+        icon {
+          url
+          title
+          description
+          width
+          height
+        }
+      }
+    }
+  }
+}
+`;
+export const GetSiteMeta = `query GetSiteMeta {
+  person(id: "2dHdqDmhdvsyfrhkYq7G8n") {
+    firstName
+    lastName
+    headline
+  }
+}`;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,50 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-
+import { GetSiteMeta } from './graphql.query';
+import { Person } from './types';
+import { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'], variable: '--inter' });
 
-export const metadata = {
-  title: 'John Hodge',
-  description: 'Product professional and adtech nerd.',
+export type Headers = {
+  Authorication: string;
+  'Content-Type': string;
 };
+
+const headers = {
+  Authorization: `Bearer ${process.env.PUBLIC_CONTENTFUL_CONTENT_DELIVERY_TOKEN}`,
+  'Content-Type': 'application/json',
+};
+
+export async function generateMetadata() {
+  const graphql = JSON.stringify({
+    query: GetSiteMeta,
+    variables: {},
+  });
+  const response = await fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.PUBLIC_CONTENTFUL_SPACE_ID}/environments/staging`,
+    {
+      method: 'POST',
+      body: graphql,
+      headers: headers,
+    }
+  );
+  const json = await response.json();
+  const data: Person = json.data.person;
+
+  const metadata: Metadata = {
+    title: `${data.firstName} ${data.lastName}`,
+    description: data.headline,
+    openGraph: {
+      type: 'website',
+      url: 'https://www.johnhodge.com',
+      title: `${data.firstName} ${data.lastName}`,
+      description: data.headline,
+      siteName: `${data.firstName} ${data.lastName}`,
+    },
+  };
+
+  return metadata;
+}
 
 export default function RootLayout({
   children,
