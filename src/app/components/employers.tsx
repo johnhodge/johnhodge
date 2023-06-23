544326037237317d19c26decfc9b07b03d081a14
John <18251637+johnhodge@users.noreply.github.com>
2023-06-23 19:44:43 -0400

    MAR-26 create employers compoenent

--- src/app/components/employers.tsx
+++ src/app/components/employers.tsx
@@ -1,12 +1,42 @@
 import type { Person } from '../types';
 import Article from '../templates/article';
+import GlobalCard from './shared/card';
+import { GlobalButtonSettings } from './shared/button';
 
-export default function Client(data: Person) {
+export default async function Employers(data: Person) {
+  const button: GlobalButtonSettings = {
+    text: 'Read more',
+    color: 'primary',
+    size: 'small',
+    link: '/#',
+    width: 'fit',
+  };
   return (
     <Article
+      id='employers'
       headline='Employers'
-      subhead='Let’s see where I’ve been'
+      subhead="Let's see where I've been"
       button={false}
-      data={data}></Article>
+      data={data}>
+      <div className='flex flex-wrap gap-4'>
+        {data.employment.items.map((employer) => (
+          <div
+            key={employer.clientsCollection.items[0].name}
+            className='shrink grow md:basis-1/3'>
+            <GlobalCard
+              key={employer.clientsCollection.items[0].name}
+              logo={employer.clientsCollection.items[0].logo}
+              header={employer.jobTitle}
+              subheader={employer.clientsCollection.items[0].name}
+              longDescription={employer.headline}
+              body={employer.body}
+              verticalLine={false}
+              horizontalLine={true}
+              button={button}
+            />
+          </div>
+        ))}
+      </div>
+    </Article>
   );
 }
