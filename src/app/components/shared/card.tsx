ad0821451e4ddc8fd2c2a4e4e4bd362049774183
John <18251637+johnhodge@users.noreply.github.com>
2023-06-23 12:23:45 -0400

    MAR-25 style global card

--- src/app/components/shared/card.tsx
+++ src/app/components/shared/card.tsx
@@ -6,10 +6,10 @@ import { ButtonSettings } from './button';
 type GlobalCard = {
   logo?: MediaImage;
   icon?: MediaImage;
-  header: string;
-  subheader: string;
-  shortDescription: string;
-  longDescription: string;
+  header?: string;
+  subheader?: string;
+  shortDescription?: string;
+  longDescription?: string;
   button?: ButtonSettings;
   verticalLine: boolean;
   horizontalLine: boolean;
@@ -17,7 +17,7 @@ type GlobalCard = {
 
 export default function GlobalCard(props: GlobalCard) {
   return (
-    <section>
+    <section className='bg-gradient-to-t to-gray-0 from-gray-100 shadow-md p-8 rounded-3xl flex flex-col gap-4'>
       {props.logo?.url ? (
         <figure>
           <Image
@@ -34,35 +34,68 @@ export default function GlobalCard(props: GlobalCard) {
         ''
       )}
 
-      <div>
-        {props.icon?.url ? (
-          <figure>
-            <Image
-              src={props.icon.url}
-              height={props.icon.height}
-              width={props.icon.width}
-              title={props.icon.title}
-              alt={props.icon.description}
-            />
-            <figcaption className='hidden'>{props.icon.description}</figcaption>
-          </figure>
+      <div className='flex flex-col gap-4'>
+        <hgroup className='flex gap-2'>
+          {props.icon?.url ? (
+            <figure className='flex flex-col justify-center'>
+              <Image
+                src={props.icon.url}
+                height={props.icon.height}
+                width={props.icon.width}
+                title={props.icon.title}
+                alt={props.icon.description}
+              />
+              <figcaption className='hidden'>
+                {props.icon.description}
+              </figcaption>
+            </figure>
+          ) : (
+            ''
+          )}
+          {props.subheader && props.shortDescription ? (
+            <div
+              className={`flex flex-col justify-center gap-4 ${
+                props.verticalLine ? 'border-l border-gray-950 pl-2' : ''
+              }`}>
+              {props.subheader ? (
+                <h3
+                  className='text-2xl font-extrabold
+              lg:text-4xl
+              xl:text-5xl'>
+                  {props.subheader}
+                </h3>
+              ) : (
+                ''
+              )}
+              {props.shortDescription ? <p>{props.shortDescription}</p> : ''}
+            </div>
+          ) : (
+            ''
+          )}
+        </hgroup>
+
+        {props.horizontalLine ? <hr className='border-gray-950' /> : ''}
+        {props.header ? (
+          <h2
+            className='text-5xl font-black 
+        lg:text-7xl
+        xl:text-8xl'>
+            {props.header}
+          </h2>
         ) : (
           ''
         )}
-        <div>
-          <h3>{props.subheader}</h3>
-          <p>{props.shortDescription}</p>
-        </div>
-        <h2>{props.header}</h2>
-        <p>{props.longDescription}</p>
+        {props.longDescription ? <p>{props.longDescription}</p> : ''}
         {props.button ? (
-          <Button
-            text={props.button.text}
-            size={props.button.size}
-            color={props.button.color}
-            width={props.button.width}
-            link={props.button.link}
-          />
+          <div className='self-auto'>
+            <Button
+              text={props.button.text}
+              size={props.button.size}
+              color={props.button.color}
+              width={props.button.width}
+              link={props.button.link}
+            />
+          </div>
         ) : (
           ''
         )}
