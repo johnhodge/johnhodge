6b0690a39bf3d83dd5fe0134693af7ca4a16d0ab
John <18251637+johnhodge@users.noreply.github.com>
2023-06-24 13:59:38 -0400

    MAR-28 build skills component

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -1,5 +1,6 @@
 import type { Person } from '../types';
 import Article from '../templates/article';
+import GlobalCard from './shared/card';
 
 export default function Client(data: Person) {
   return (
@@ -8,6 +9,24 @@ export default function Client(data: Person) {
       headline='Skills'
       subhead='Let’s see what I know'
       button={false}
-      data={data}></Article>
+      data={data}>
+      <div className='flex flex-wrap gap-4'>
+        {data.skills.items.map((skill) => (
+          <div
+            key={skill.title.replace(' ', '').toLowerCase()}
+            className='shrink grow md:basis-1/3 xl:basis-1/4'>
+            <GlobalCard
+              key={skill.title.replace(' ', '').toLowerCase()}
+              icon={skill.icon}
+              iconAlign='center'
+              subheader={skill.title}
+              longDescription={skill.headline}
+              verticalLine={false}
+              horizontalLine={true}
+            />
+          </div>
+        ))}
+      </div>
+    </Article>
   );
 }
