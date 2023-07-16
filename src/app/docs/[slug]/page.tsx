9b6066188222379e85786d5f59d8aaf731f48cf5
John <18251637+johnhodge@users.noreply.github.com>
2023-07-16 11:05:53 -0400

    MAR-60 minor SEO and layout changes

--- src/app/docs/[slug]/page.tsx
+++ src/app/docs/[slug]/page.tsx
@@ -2,17 +2,19 @@ import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
 import { MDXRemote } from 'next-mdx-remote/rsc';
-import H3, { TOC2, TOC3 } from '../components/body';
 import { cwd } from 'process';
-import { Metadata } from 'next';
 import { ReactNode } from 'react';
-import H2 from '../components/body';
+import { Metadata } from 'next';
+import Link from 'next/link';
 
-const components = {
-  h1: 'h1',
-  h2: 'h1',
-  h3: 'h1',
-};
+import H2, { H3, TOC2, TOC3 } from '../components/body';
+const postsDirectory = `${cwd()}/documentation`;
+
+// const components = {
+//   h1: 'h1',
+//   h2: 'h1',
+//   h3: 'h1',
+// };
 
 type Params = {
   params: {
@@ -20,20 +22,30 @@ type Params = {
   };
 };
 
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const realSlug = params.slug.replace(/\.mdx$/, '');
+  const fullPath = join(postsDirectory, `${realSlug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  const { data, content } = matter(fileContents);
+
+  return {
+    title: data.title,
+  };
+}
+
 export default function Docs({ params }: Params) {
-  const postsDirectory = `${cwd()}/_docs`;
   const realSlug = params.slug.replace(/\.mdx$/, '');
   const fullPath = join(postsDirectory, `${realSlug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
   return (
-    <section>
+    <section className='mx-2 border-b border-b-slate-800'>
       <article className='grid grid-cols-12 grid-rows-1 py-16 gap-4'>
         <aside className='hidden md:block col-span-3'>
-          <div className='sticky top-16'>
+          <div className='sticky top-32'>
             <p className='pb-4 text-xl font-black'>Concepts</p>
-            <ul className='max-h-[calc(100dvh-110px)] overflow-y-auto prose'>
+            <ul className='max-h-[calc(75dvh-110px)] overflow-y-auto prose'>
               <li>hey there</li>
               <li>hey there</li>
               <li>hey there</li>
@@ -70,10 +82,17 @@ export default function Docs({ params }: Params) {
               <li>hey there</li>
               <li>hey there</li>
             </ul>
+            <Link
+              href={`https://github.com/johnhodge/johnhodge/documentation/zello-world.mdx.tsx`}
+              className='pt-4 border-t bg-gray-0 block border-t-slate-200'>
+              Edit on GitHub
+            </Link>
           </div>
         </aside>
         <section className='col-span-12 prose max-w-none prose-headings:font-black prose-a:no-underline md:col-span-7'>
           <div className='prose prose-zinc prose-headings:font-black'>
+            <h1>{data.title}</h1>
+            <p>Last updated by: {data.author.name}</p>
             <MDXRemote
               source={content}
               components={{
@@ -88,9 +107,9 @@ export default function Docs({ params }: Params) {
           </div>
         </section>
         <aside className='hidden md:block col-span-2'>
-          <div className='sticky top-16'>
+          <div className='sticky top-32'>
             <p className='pb-4 text-xl font-black'>On this page</p>
-            <ul className='max-h-[calc(100dvh-110px)] overflow-y-auto prose prose-headings:font-black'>
+            <ul className='max-h-[calc(75dvh-110px)] overflow-y-auto prose prose-headings:font-black'>
               <MDXRemote
                 source={content}
                 components={{
