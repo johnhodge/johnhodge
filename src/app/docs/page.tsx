5c2ab2d9cf2e9da234c3ff046870631a61ed1e2d
John <18251637+johnhodge@users.noreply.github.com>
2023-07-24 17:20:29 -0400

    MAR-58 create page and index.mdx for docs

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,5 +1,98 @@
+import GlobalCard from '@/app/components/shared/card';
+import { readFileSync, readdirSync } from 'fs';
+import matter from 'gray-matter';
+import { Metadata } from 'next';
+import { join } from 'path';
 import { cwd } from 'process';
+import Article from '../templates/article';
+import { GlobalButtonSettings } from '../components/shared/button';
 
-export default function Docs() {
-  return cwd();
+const docsDirectoryPath = join(cwd(), 'documentation');
+export async function generateMetadata() {
+  const MDXFilePath = join(docsDirectoryPath, '_index.mdx');
+  const fileContents = readFileSync(join(MDXFilePath));
+  const { data } = matter(fileContents);
+  const metadata: Metadata = {
+    title: data.title,
+  };
+
+  return metadata;
+}
+
+type SubPageData = {
+  filename: string;
+  title: string;
+  excerpt: string;
+  icon: string;
+};
+
+function createButton(link: string): GlobalButtonSettings {
+  return {
+    size: 'small',
+    width: 'fit',
+    color: 'primary',
+    link: link,
+    text: 'Read more',
+  };
+}
+
+export default async function Page() {
+  const subPages: Record<string, SubPageData> = {};
+  const subPageDirs = readdirSync(docsDirectoryPath);
+  subPageDirs
+    .filter((file: string) => file != '_index.mdx')
+    .map((subPageDir: string) =>
+      readdirSync(join(docsDirectoryPath, subPageDir))
+        .filter((page) => page == '_index.mdx')
+        .map(
+          (subPage) =>
+            (subPages[subPageDir] = {
+              filename: subPage,
+              title: matter(
+                readFileSync(
+                  join(docsDirectoryPath, subPageDir, subPage),
+                  'utf-8'
+                )
+              ).data.title,
+              excerpt: matter(
+                readFileSync(
+                  join(docsDirectoryPath, subPageDir, subPage),
+                  'utf-8'
+                )
+              ).data.excerpt,
+              icon: matter(
+                readFileSync(
+                  join(docsDirectoryPath, subPageDir, subPage),
+                  'utf-8'
+                )
+              ).data.icon,
+            })
+        )
+    );
+
+  return (
+    <Article
+      id={'docs'}
+      headline={
+        matter(readFileSync(join(docsDirectoryPath, '_index.mdx'))).data.title
+      }
+      subhead={
+        matter(readFileSync(join(docsDirectoryPath, '_index.mdx'))).data.excerpt
+      }>
+      <div className='grid grid-cols-6 gap-4'>
+        {Object.keys(subPages).map((page) => (
+          <div className='col-span-6 lg:col-span-3 xl:col-span-2'>
+            <GlobalCard
+              verticalLine={false}
+              horizontalLine={true}
+              iconId={subPages[page].icon}
+              subheader={subPages[page].title}
+              longDescription={subPages[page].excerpt}
+              button={createButton(join('/docs', page))}
+            />
+          </div>
+        ))}
+      </div>
+    </Article>
+  );
 }
