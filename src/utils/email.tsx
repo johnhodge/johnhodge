94be518779c9df839d99009c8550babb28da2af0
John <18251637+johnhodge@users.noreply.github.com>
2023-08-05 15:19:12 -0400

    MAR-49 update email util to set email data with new type

--- src/utils/email.tsx
+++ src/utils/email.tsx
@@ -1,19 +1,19 @@
 'use server';
 import { Email } from '@/app/components/email-basic-follow-up';
-import { FormValueTypes } from '@/app/components/form';
+import { EmailData, SendEmailSettings } from '@/app/types';
 import { render } from '@react-email/render';
 import sendgrid from '@sendgrid/mail';
 
 sendgrid.setApiKey(process.env.PUBLIC_SENDGRID_API_KEY ?? '');
-export default async function SendEmail(props: FormValueTypes) {
+export default async function SendEmail(props: EmailData) {
   const emailHtml = render(<Email {...props} />, {
     pretty: true,
   });
 
-  const options = {
-    from: 'John Hodge <info@johnhodge.com>',
-    to: props.email,
-    subject: `Thanks for reaching out, ${props.firstName}!`,
+  const options: SendEmailSettings = {
+    from: `${props.sender.name} <${props.sender.email}>`,
+    to: props.recipient.email,
+    subject: props.subject,
     html: emailHtml,
   };
   await sendgrid.send(options);
