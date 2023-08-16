712d7b7dbdbe8f5e97fd73ecd85f56851627f2aa
John <18251637+johnhodge@users.noreply.github.com>
2023-08-16 14:03:50 -0400

    MAR-97 set reactStrictMode to true

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  profiler: true,
+  reactStrictMode: true,
   experimental: {
     serverActions: true,
   },
