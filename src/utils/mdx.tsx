1f0f0146d9881d9cf4d09d730609873952b1eff6
John <18251637+johnhodge@users.noreply.github.com>
2023-08-16 11:10:48 -0400

    MAR-97 adjust types for cleaner data shaping create mdx func just for getting data use new mdx func in doc template w/ spread

--- src/utils/mdx.tsx
+++ src/utils/mdx.tsx
@@ -1,10 +1,22 @@
+import { PostData } from '@/app/types';
 import { readFileSync, readdirSync } from 'fs';
 import matter from 'gray-matter';
 
-export function GetDataContent(fileLocation: string) {
+export function GetMdxDataContent(fileLocation: string) {
   const fileContents = readFileSync(fileLocation);
   const { data, content } = matter(fileContents);
   return { data, content };
+}
+export function GetMdxData(fileLocation: string): PostData {
+  const fileContents = readFileSync(fileLocation);
+  const { data } = matter(fileContents);
+  return {
+    title: data.title,
+    excerpt: data.excerpt,
+    icon: data.icon,
+    date: data.date,
+    author: { firstName: data.firstName, lastName: data.lastName },
+  };
 }
 
 export function GetSubFolders(directoryLocation: string) {
