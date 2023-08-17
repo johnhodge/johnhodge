7b67aeb1f22774555ab0c39049ff1b1c73995ec5
John Hodge <18251637+johnhodge@users.noreply.github.com>
2023-08-17 16:59:41 -0400

    MAR-98 fix top spacing on mobile TOC icon (#146)

--- src/app/resources/[documentation]/components/toc.tsx
+++ src/app/resources/[documentation]/components/toc.tsx
@@ -21,7 +21,7 @@ export default function GlobalTOC(props: TOCData) {
   }
   return (
     <>
-      <div className='md:hidden sticky z-10 top-32'>
+      <div className='md:hidden sticky z-10 top-20'>
         <span
           onClick={handleClick}
           className='font-emoji rounded-lg min-w-fit inline-block px-1 text-3xl text-center border border-solid bg-gradient-to-b border-gray-400 from-gray-100 to-gray-50 text-gray-700'>
