f8cd0df1313d7fde3faa0c6cf299b8d85247c7fe
John <18251637+johnhodge@users.noreply.github.com>
2023-08-20 23:26:42 -0400

    MAR-97 config generateStaticParams rename dynamic routes add custom 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,33 @@
+import GlobalButton from './components/shared/button';
+import Article from './templates/article';
+
+export default function NotFound() {
+  return (
+    <Article id='error-404' headline='404' subhead='Resource not found'>
+      <div className='flex flex-col gap-4'>
+        <p>
+          We can't find the resource you requested. Please double check the URL
+          and try again.
+        </p>
+        <div className='flex gap-4'>
+          <GlobalButton
+            text='Schedule a consultation'
+            href='/#contact'
+            size='small'
+            color='primary'
+            width='fit'
+            buttonType='route'
+          />
+          <GlobalButton
+            text='Return home'
+            href='/'
+            size='small'
+            color='gray'
+            width='fit'
+            buttonType='route'
+          />
+        </div>
+      </div>
+    </Article>
+  );
+}
