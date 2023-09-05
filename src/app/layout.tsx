d1c24fdb362c0cf2bf0bc8f097466573cd6f0784
John Hodge <18251637+johnhodge@users.noreply.github.com>
2023-09-05 10:21:03 -0400

    Fix misaligned linked header elements and improve wording (#171)
    * MAR-104 move link header outside the h tag
    
    * MAR-104 update font styling
    move class to html element
    set font in tailwind config
    
    * minor wording updates
    
    * minor wording updates on app store data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en' className='antialiased'>
+    <html
+      lang='en'
+      className={`${inter.variable} ${firaCode.variable} antialiased`}>
       <Script id='google-tag-manager' strategy='afterInteractive'>
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -28,7 +30,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-P34JZK')`}
       </Script>
-      <body className={`${inter.variable} ${firaCode.variable}`}>
+      <body>
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe
